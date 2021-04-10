4be87eb37020e88b3fd8bd568c2424d6329d0e52
Deep Makadiya <deepmac.25@gmail.com>
2021-04-10 20:43:57 +0530

    style: theme update

--- src/Theme/Colors.jsx
+++ src/Theme/Colors.jsx
@@ -1,10 +1,12 @@
 // export const outerShadow = '3px 3px 3px #222327, -3px -3px 3px #363636';
 // export const outerShadow = '3px 3px 3px #17042c, -3px -3px 3px #1e1a3d';
-export const outerShadow = '3px 3px 3px #140425, -3px -3px 3px #1b1642';
+// export const outerShadow = '3px 3px 3px #140425, -3px -3px 3px #1b1642';
+export const outerShadow = '3px 3px 6px #0d0d0d, -3px -3px 6px #353535';
 
 // export const innerShadow = 'inset 3px 3px 3px #222327, inset -3px -3px 3px #363636';
-// export const innerShadow = 'inset 3px 3px 3px #17042c, inset -3px -3px 3px #1e1a3d';
-export const innerShadow = 'inset 3px 3px 3px #140425, inset -3px -3px 3px #1b1642';
+// export const innerShadow = 'inset 3px 3px 3px #17042c, inset -3px -3px 3px #1e13ad';
+// export const innerShadow = 'inset 3px 3px 3px #140425, inset -3px -3px 3px #1b1642';
+export const innerShadow = 'inset 3px 3px 6px #0d0d0d, inset -3px -3px 6px #353535';
 
 export const bgBlack900 = '#ffffff';
 export const bgBlack100 = '#353535';
@@ -13,4 +15,6 @@ export const textBlack900 = '#ffffff';
 export const textBlack700 = '#ffffff';
 export const textBlack600 = '#bbbbbb';
 
-export const bgBlue = '#120b35';
+// export const bgBlue = '#120b35';
+// export const bgBlue = '#1533b3';
+export const bgDark = '#212121';
--- src/Theme/Theme.jsx
+++ src/Theme/Theme.jsx
@@ -1,39 +1,38 @@
 import React from 'react';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { green, purple } from '@material-ui/core/colors';
-import { bgBlack50, textBlack600, textBlack700, bgBlue } from './Colors';
+import { bgDark } from './Colors';
+
+// const darkTheme = createMuiTheme({
+//   palette: {
+//     type: 'dark',
+//     primary: green,
+//     secondary: purple,
+//     background: {
+//       paper: bgBlack50,
+//       default: bgBlack50,
+//     },
+//     text: {
+//       primary: textBlack700,
+//       secondary: textBlack600,
+//     },
+//   },
+//   typography: {
+//     fontFamily: 'Poppins',
+//   },
+// });
 
 const darkTheme = createMuiTheme({
   palette: {
     type: 'dark',
-    primary: green,
-    secondary: purple,
-    background: {
-      paper: bgBlack50,
-      default: bgBlack50,
-    },
-    text: {
-      primary: textBlack700,
-      secondary: textBlack600,
-    },
-  },
-  typography: {
-    fontFamily: 'Poppins',
-  },
-});
-
-const blueTheme = createMuiTheme({
-  palette: {
-    type: 'dark',
     primary: {
-      // light: '#f0c38e',
       light: '#eba14d',
       main: '#ff9c07',
     },
     secondary: purple,
     background: {
-      paper: bgBlue,
-      default: bgBlue,
+      paper: bgDark,
+      default: bgDark,
     },
     text: {
       primary: '#ffffff',
@@ -63,7 +62,7 @@ const blueTheme = createMuiTheme({
 
 const Theme = (props) => {
   const { children } = props;
-  return <ThemeProvider theme={blueTheme}>{children}</ThemeProvider>;
+  return <ThemeProvider theme={darkTheme}>{children}</ThemeProvider>;
 };
 
 export const withTheme = (Component) => {
